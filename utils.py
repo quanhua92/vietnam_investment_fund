1243f486de2ca55f672fa0dede6c29e654330e8f
quanhua92 <quanhua92@gmail.com>
2021-10-19 13:02:49 +0700

    Oct 19 2021

---
+++ utils.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def get_all_products():
+    url = "https://api.fmarket.vn/res/products/filter"
+    data = {"types":["NEW_FUND","TRADING_FUND"],"issuerIds":[],"page":1,"pageSize":1000,"fundAssetTypes":[],"bondRemainPeriods":[],"searchField":""}
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    x = requests.post(url, json=data, headers=headers)
+    return json.loads(x.text)
+
+def get_history(product_id):
+    url = "https://api.fmarket.vn/res/product/get-nav-history"
+    toDate = datetime.now().strftime("%Y%m%d")
+    data = {"isAllData":1,"productId":product_id,"fromDate": None, "toDate": toDate}
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    x = requests.post(url, json=data, headers=headers)
+    return json.loads(x.text)
